2afa61d5b764a44a3c4106e1a3a257b9dd1a1e40
rexyray <crm5acastrod@gmail.com>
2022-11-16 12:30:19 -0300

    dasd

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -2,7 +2,6 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null; 
-nbjjnjnk
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
 
