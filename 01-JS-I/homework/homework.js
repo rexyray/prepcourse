d0ee9250f66ccb3dc67a927c0cd28087d9727bfa
rexyray <crm5acastrod@gmail.com>
2022-11-16 12:31:04 -0300

    HOLI

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -2,6 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null; 
+sADSAFAS
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
 
